f4f025cfff355db6cde25620051ae2e5b835bd5f
Palu17 <10946432@polimi.it>
2024-11-04 19:43:05 +0100

    agg scritt il codice

--- sketch.js
+++ sketch.js
@@ -1,17 +1,59 @@
-function preload() {
-  // put preload code here
-}
-
 function setup() {
   createCanvas(windowWidth, windowHeight);
-  // put setup code here
-  const message =
-    "This is a template repository\nfor the course Laboratorio di Computergrafica\nCommunication Design, Politecnico di Milano";
-  textAlign(CENTER, CENTER);
-  textSize(16);
-  text(message, width / 2, height / 2);
+  rectMode(CENTER);
+  noLoop();
 }
 
 function draw() {
-  // put drawing code here
+  background("black");
+  
+  const gridSize = 5;
+  const cellSize = min(width, height) / gridSize; // dimensione delle celle
+  const squareSize = cellSize * 0.9; //ridotta un poco pr avere del margine
+  
+  //centro la griglia:
+  const startX = (width - cellSize * gridSize) / 2;
+  const startY = (height - cellSize * gridSize) / 2; 
+
+  for (let i = 0; i < gridSize * gridSize; i++) {
+    const col = i % gridSize; // ottengo la colonna con il modlo
+    const row = floor(i / gridSize); // ottengo la riga
+    const centerX = startX + col * cellSize + cellSize/2; //calcolo il centro X
+    const centerY = startY + row * cellSize + cellSize/2; // calcolo il centro Y
+    
+    drawCell(centerX, centerY, squareSize); //chiamo la funzione per disegnare la cella
+  }
+}
+
+function drawCell(x, y, size) {
+  const circleSize = size/4; //dimensioni del cerchio
+  
+  // Cerchio centrale nero (non so come toglierlo senza sfanculare tutto)
+  fill("black");
+  stroke("black");
+  circle(x, y, circleSize);
+  
+  // queste sono le linee
+  stroke("#21FFFF");
+  const numLines = 50;
+  const lineLength = size/4;
+  const maxAngle = PI/6;
+  
+  for (let i = 0; i < numLines; i++) {
+    const baseAngle = random(TWO_PI);
+    const startX = x + cos(baseAngle) * (circleSize/2);
+    const startY = y + sin(baseAngle) * (circleSize/2);
+    const tangentAngle = baseAngle + HALF_PI + random(-maxAngle, maxAngle);
+    
+    line(
+      startX, 
+      startY, 
+      startX + cos(tangentAngle) * lineLength,
+      startY + sin(tangentAngle) * lineLength
+    );
+  }
+}
+
+function mousePressed() {
+  redraw();
 }
